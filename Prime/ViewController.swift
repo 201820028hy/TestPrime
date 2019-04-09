b4d3837b4846d65fab0ccf5e9fbf3ae565ed062c
dit03 <dit@imac-4.local>
2019-04-09 14:25:24 +0900

    Initial Commit

---
+++ Prime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prime
+//
+//  Created by dit03 on 2019. 4. 9..
+//  Copyright © 2019년 201820028. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
