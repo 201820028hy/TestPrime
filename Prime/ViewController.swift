a948ccde15f546fdeaf200d8b7754e83f02404c9
dit03 <dit@imac-4.local>
2019-04-09 15:47:19 +0900

    Number of Prime

--- Prime/ViewController.swift
+++ Prime/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtPrime: UITextField!
+    
+    @IBOutlet weak var lbCheck: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lbCheck.text = "Please Input Number"
     }
 
-
+    @IBAction func btnCheck(_ sender: Any) {
+        
+        var isPrime = true
+        var num:Int! = Int(txtPrime.text!)
+        
+        if (num != 1 && num != 2) {
+            for i in 2..<num {
+                if(num % i == 0) {
+                    isPrime = false
+                }
+            }
+        } else if(num == 1) {
+            isPrime = false
+        } else {
+            isPrime = true
+        }
+        
+        if(isPrime == true) {
+            lbCheck.text = "Number is Prime"
+            txtPrime.text = ""
+        } else {
+            lbCheck.text = "Number is Not Prime"
+            txtPrime.text = ""
+        }
+        
+    }
+    
 }
 
